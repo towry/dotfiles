ac860381e382ed199c803e33cbbb8d03dba30edc
towry <tovvry@gmail.com>
2017-01-30 21:06:10 +0800

    Finish scripts

---
+++ install/conf.sh
@@ -0,0 +1,5 @@
+ROOT_DIR="$HOME/.dotfiles"
+LINK_DIR="$ROOT_DIR/link"
+MAP_FILE="$ROOT_DIR/install/map.txt"
+
+source "$ROOT_DIR/install/common.sh"
---
+++ install/remove.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# remove file from link directory
+
+source "$HOME/.dotfiles/install/conf.sh"
+
+# remove record from map.txt
+function remove_from_map {
+	sed -i.bak "\#$1#d" "$MAP_FILE"
+}
+
+if [[ $# -eq 0 ]]; then
+	echo "usage: $0 [file]"
+	exit 0
+fi
+
+fname=$(realpath "$1")
+
+if [[ ! -f "$fname" ]]; then
+	echo "$fname file not exists."
+	echo "exit."
+	exit 0
+fi
+
+DEST_NAME=$HOME/${fname#"$LINK_DIR/"}
+
+if [[ -f $DEST_NAME ]]; then
+	echo "do you want to restore '$DEST_NAME'?"
+	cmn_ask_to_continue
+fi
+# else: continueo
+
+# move file
+mv $fname $DEST_NAME
+cmn_echo_info "mv {$fname} => {$DEST_NAME}"
+
+# remove linked file.
+if [[ -f $fname ]]; then
+	rm $fname
+fi
+
+remove_from_map $DEST_NAME
