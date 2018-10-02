c2b037b591b12f3cd1331de7b8d0c078c402e24f
towry <tovvry@gmail.com>
2018-10-02 23:41:04 +0800

    up

--- source/debian_zshrc.sh
+++ source/debian_zshrc.sh
@@ -1,2 +1,8 @@
 
+# alias
 alias subl=subl.exe
+
+# exports
+export DOCKER_HOST=tcp://192.168.99.100:2376
+export DOCKER_CERT_PATH=/mnt/c/Users/towry/.docker/machine/certs
+export DOCKER_TLS_VERIFY=1
