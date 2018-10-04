de74136979ee4889d75e5cf71ebf3ec25b656da4
towry <tovvry@gmail.com>
2018-10-04 23:15:18 +0800

    up

--- source/debian_zshrc.sh
+++ source/debian_zshrc.sh
@@ -3,3 +3,6 @@
 alias subl=subl.exe
 
 # exports
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
+export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
