fdfaed46403e59da2a95f927e2653fba559e24c0
towry <tovvry@gmail.com>
2019-05-11 17:55:24 +0800

    Fix path

--- source/zsh/path.sh
+++ source/zsh/path.sh
@@ -8,10 +8,12 @@ _PATH_STR="$PATH:/usr/local/opt/openssl/bin\
 :$HOME/.cargo/bin"
 
 # nvm
-if [ -f "~/.nvm/alias/default" ]; then
+if [ -f ~/.nvm/alias/default ]; then
     DEF_NODE_VER="$(< ~/.nvm/alias/default)"
     DEF_NODE_BIN_PATH=":$HOME/.nvm/versions/node/$DEF_NODE_VER/bin"
-    _PATH_STR+=DEF_NODE_BIN_PATH
+    _PATH_STR+=$DEF_NODE_BIN_PATH
+else
+    echo "NO node"
 fi
 
 # --------- END --------
