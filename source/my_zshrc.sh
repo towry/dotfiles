aceff77e4ac19b43a66e8018986a6533a77a3f3d
towry <tovvry@gmail.com>
2021-05-08 11:42:06 +0800

    up

--- source/my_zshrc.sh
+++ source/my_zshrc.sh
@@ -15,7 +15,6 @@ alias gac='echo "$(tput bold)$(tput setaf 3)warning: be carefull$(tput sgr0)" &&
 alias gcz='echo "$(tput bold)$(tput setaf 3)warning: be carefull$(tput sgr0)" && git add . && git cz'
 alias gtail="git rev-list --all | tail"
 alias xmerge="git merge --no-ff"
-alias gprune="for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done"
 alias xmerged="git branch --merged master"
 alias g="git"
 if type nvim > /dev/null; then
